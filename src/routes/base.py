484edd97cdf735c67185083a93773db4ffb4a731
NawrasFarhat <nawrasfarhat123@gmail.com>
2026-01-01 04:53:17 +0300

    restructure project and add config system

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, APIRouter
 import os
+from helpers.config import Settings
+
 base_router=APIRouter(
     prefix="/api/v1",
     tags=["api_v1"]
 )
 
 @base_router.get("/")
-def welcome():
-    app_name=os.getenv('APP_NAME')
-    app_version=os.getenv('APP_VEERSION')
+async def welcome():
+    app_settings = Settings()
+    
+    app_name=app_settings.APP_NAME
+    app_version=app_settings.APP_VERSION
     return {
         "app_name": app_name,
         "app_version":app_version,
