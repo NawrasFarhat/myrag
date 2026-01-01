a686bf05844567ee9dc19e6fd8bcab54dfabebd5
NawrasFarhat <nawrasfarhat123@gmail.com>
2026-01-02 02:02:07 +0300

    add signal

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, UploadFile
 from helpers.config import get_settings, Settings
-
+from controllers.DataController import DataController
 data_router = APIRouter(
     prefix="/api/v1/data", 
     tags=["api_v1","data"],
@@ -10,4 +10,11 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
 app_settings: Settings =Depends(get_settings)):
 
-#validate the file properties
+  #validate the file properties
+
+  is_valid, resulte_signal=DataController().validate_uploaded_file(file=file)
+
+  return {
+    "signal":resulte_signal
+  }
+ 
