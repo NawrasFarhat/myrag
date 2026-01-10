b9827e4ef4c3ebe4661e379f64d68a195082f198
NawrasFarhat <nawrasfarhat123@gmail.com>
2026-01-10 17:19:43 +0300

    add mongodb config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,9 +4,13 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+
     FILE_ALLOWD_TYPE: list
     FILE_MAX_SIZE: int 
     FILE_DEFFAULT_CHUNK_SIZE: int
+    
+    MONGODB_URL: str
+    MONGODB_DATABASE:str
 
 
     model_config = SettingsConfigDict(
